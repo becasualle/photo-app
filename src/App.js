ad65b3f11c4cf45995cdb548d3e00c75af6cf7b4
Mark <mark.nelubin@gmail.com>
2021-10-13 15:41:56 +0300

    global context setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo';
 import { useGlobalContext } from './context'
 
 function App() {
@@ -8,22 +7,7 @@ function App() {
   console.log(names)
 
   return <main>
-    <section className="search">
-      <form className="search-form">
-        <input type="text" placeholder="поиск" className="form-input" value={{/*query*/ }} onChange={{/*e => setQuery(e.target.value)*/ }} />
-        <button type="submit" className="submit-btn" onClick={{/*handleSubmit*/ }}>
-          <FaSearch />
-        </button>
-      </form>
-      <button onClick={{/*handleLiked*/ }}>liked</button>
-    </section>
-    <section className="photos">
-      <div className="photos-center">
-        {/* photos.map((photo, idx) => <Photo key={photo.id} item={photo} setLikedPhotos={setLikedPhotos} photos={photos} likedPhotos={likedPhotos} />)} */}
 
-      </div>
-      {/* {loading && <h2 className='loading'>Loading...</h2>} */}
-    </section>
   </main>
 }
 
